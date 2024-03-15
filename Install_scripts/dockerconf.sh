16ea8f59cc3a6c45078b8d4d8da200e22dbffa0a
Kitsune <48340051+ch1ru@users.noreply.github.com>
2024-03-15 16:47:47 +0000

    Update dockerconf.sh

--- Install_scripts/dockerconf.sh
+++ Install_scripts/dockerconf.sh
@@ -1,4 +1,12 @@
 #! /bin/bash
 
-sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
+# Setup Docker Repo
+sudo apt update -y
+sudo apt install -y ca-certificates curl gnupg lsb-release
+sudo mkdir -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+# Install Docker Engine
+sudo apt-get update -y 
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
