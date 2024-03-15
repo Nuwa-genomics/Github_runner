f0040878106977ffc5266467a2e52d7e8153c919
Kitsune <48340051+ch1ru@users.noreply.github.com>
2024-03-15 15:02:13 +0000

    Update and rename config.sh to start.sh

--- Docker/start.sh
+++ Docker/start.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+REPO=$REPO
+ACCESS_TOKEN=$TOKEN
+
 REG_TOKEN=$(curl -X POST -H "Authorization: token ${ACCESS_TOKEN}" -H 
 "Accept: application/vnd.github+json" https://api.github.com/repos/${REPO}/actions/runners/registration-token | jq .token --raw-output)
 
@@ -8,7 +11,9 @@ cd /home/docker/actions-runner
 ./config.sh --url https://github.com/${REPO} --token ${REG_TOKEN}
 
 cleanup() {
-echo "Removing runner..."    ./config.sh remove --unattended --token ${REG_TOKEN}
+echo "Removing runner..." 
+./config.sh remove --unattended --token ${REG_TOKEN}
 }
+
 trap 'cleanup; exit 130' INT
 trap 'cleanup; exit 143' TERM
