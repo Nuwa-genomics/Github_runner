6ee68c1d4b42f79f3af07630f4213177a8344283
Kitsune <48340051+ch1ru@users.noreply.github.com>
2024-03-15 18:31:56 +0000

    Update start.sh

--- Runner_scripts/start.sh
+++ Runner_scripts/start.sh
@@ -3,6 +3,7 @@
 REPO=$REPO
 ACCESS_TOKEN=$TOKEN
 RUNNER_VERSION="2.314.1"
+DEBIAN_FRONTEND=noninteractive
 
 #REG_TOKEN=$(curl -X POST -H "Authorization: token ${ACCESS_TOKEN}" -H "Accept: application/vnd.github+json" https://api.github.com/repos/${REPO}/actions/runners/registration-token | jq .token --raw-output)
 
