14f8eec4ae534b989b1c001de89710f45a6e0327
Kitsune <48340051+ch1ru@users.noreply.github.com>
2024-03-15 18:33:41 +0000

    Update start.sh

--- Runner_scripts/start.sh
+++ Runner_scripts/start.sh
@@ -5,6 +5,9 @@ ACCESS_TOKEN=$TOKEN
 RUNNER_VERSION="2.314.1"
 DEBIAN_FRONTEND=noninteractive
 
+apt-get update -y
+apt install -y --no-install-recommends curl jq build-essential libssl-dev libffi-dev python3 python3-venv python3-dev python3-pip
+
 #REG_TOKEN=$(curl -X POST -H "Authorization: token ${ACCESS_TOKEN}" -H "Accept: application/vnd.github+json" https://api.github.com/repos/${REPO}/actions/runners/registration-token | jq .token --raw-output)
 
 cd /home/docker/actions-runner
